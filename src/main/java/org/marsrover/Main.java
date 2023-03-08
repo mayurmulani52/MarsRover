bb81754e2e9beb06154b6eea33b6f7aa0acb98fc
Mayur Mulani <mayur.mulani@cognizant.com>
2023-03-08 17:11:25 +1100

    Multiple Rover deployments

--- src/main/java/org/marsrover/Main.java
+++ src/main/java/org/marsrover/Main.java
@@ -11,6 +11,8 @@ import org.marsrover.model.Rover;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -40,22 +42,35 @@ public class Main {
 
     }
 
+    private void navigateRovers(List<Rover> roverList) {
+        roverList.forEach(rover -> {
+            rover.navigateRover();
+            log.info("Rover[" + rover.getRoverId() + "] Final Position after series of commands: " + rover.getPosition().getX() + " " + rover.getPosition().getY() + " " +
+                    Direction.getDirectionByOrientation(rover.getPosition().getOrientation()));
+        });
+    }
+
     public static void main(String[] args) throws IOException {
+
         log.info("Please input the coordinates of the Plateau! Make sure its positive values only! for e.g: 2 2");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String firstLine = br.readLine();
         PlateauCoordinates.setPlateauCoordinates(firstLine);
         Main main = new Main();
-        Rover rover = null;
+        List<Rover> roverList = new ArrayList<Rover>();
         try {
-            log.info("Please input the First Rover Position split by Space! for e.g: 1 2 N");
-            String roverPositionLine = br.readLine();
-            log.info("Please input commands to Rover without Space! for e.g: LMLMLMLMM");
-            String commandInputLine = br.readLine();
-            rover = main.initRover(roverPositionLine, commandInputLine);
-            rover.navigateRover();
-            log.info("Rover[" + rover.getRoverId() + "] Final Position after series of commands: " + rover.getPosition().getX() + " " + rover.getPosition().getY() + " " +
-                    Direction.getDirectionByOrientation(rover.getPosition().getOrientation()));
+            // taking two rover inputs for the deployment. It can be increased as per the need
+            for (int i=0; i<2; i++) {
+                Rover rover = null;
+                log.info("Please input the Rover Position split by Space! for e.g: 1 2 N");
+                String roverPositionLine = br.readLine();
+                log.info("Please input commands to Rover without Space! for e.g: LMLMLMLMM");
+                String commandInputLine = br.readLine();
+                rover = main.initRover(roverPositionLine, commandInputLine);
+                roverList.add(rover);
+            }
+            br.close();
+            main.navigateRovers(roverList);
         } catch (MarsRoverInputException me) {
             log.error("Error: ", me);
         }
